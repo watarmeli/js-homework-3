164cebfa687a135b256c31dd24a50bb2e32410f9
watarmeli <watar@ukr.net>
2021-01-27 18:24:58 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Пиши код ниже этой строки
+
+const {
+  today: {
+    low: lowToday, 
+    high: highToday, 
+    icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'
+  }, 
+  tomorrow: {
+    low: lowTomorrow, 
+    high: highTomorrow, 
+    icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'
+  }
+} = forecast;
