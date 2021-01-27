ad520f7e4a8517e49713a8978a1121f5ac704893
watarmeli <watar@ukr.net>
2021-01-27 13:26:51 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const emailInputName = 'email';
+const passwordInputName = 'password';
+
+const credentials = {
+  // Пиши код ниже этой строки
+  [emailInputName]: 'henry.carter@aptmail.com',
+  [passwordInputName]: 'jqueryismyjam'
+  
+  // Пиши код выше этой строки
+};
