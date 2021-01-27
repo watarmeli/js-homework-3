a0fe104210b4c305b7c235b919ee6e7aac69b24a
watarmeli <watar@ukr.net>
2021-01-27 19:06:00 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Пиши код ниже этой строки
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
