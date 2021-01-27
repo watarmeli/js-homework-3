239adc718951b815afac92a9a4ff878a9c10dd3f
watarmeli <watar@ukr.net>
2021-01-27 12:05:42 +0200

    main

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const apartment = {
-    imgUrl: 'https://via.placeholder.com/640x480',
-    descr: 'Просторная квартира в центре',
-    rating: 4,
-    price: 2153,
-    tags: ['premium', 'promoted', 'top'],
-  };
