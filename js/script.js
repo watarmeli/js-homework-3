eb133fc9da61e53176241433d3a2d6b807593616
watarmeli <watar@ukr.net>
2021-01-27 16:14:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+// var1
+
 function countProps(object) {
   let propCount = 0;
   // Пиши код ниже этой строки
 propCount = Object.keys(object).length;
+  // Пиши код выше этой строки
+  return propCount;
+}
+
+
+// var2
+
+function countProps(object) {
+  let propCount = 0;
+  // Пиши код ниже этой строки
+	for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+        propCount += 1;   
+        };
+    };
   // Пиши код выше этой строки
   return propCount;
 }
