fa6dca087ce670a053d6c3ae6cada8a11c6d2241
watarmeli <watar@ukr.net>
2021-01-29 22:27:59 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const atTheOldToad = {
+  // Пиши код ниже этой строки
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  getPotions() {
+    return this.potions;
+  },
+  // Пиши код выше этой строки
+};
+
+console.log(atTheOldToad.getPotions());
