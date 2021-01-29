0a5c3e317294e03084cd39a45bc7354aa8eb64f4
watarmeli <watar@ukr.net>
2021-01-29 22:33:59 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const atTheOldToad = {
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  addPotion(potionName) {
+    // Пиши код ниже этой строки
+    this.potions.push(potionName);
+    return this.potions;
+    // Пиши код выше этой строки
+  },
+};
