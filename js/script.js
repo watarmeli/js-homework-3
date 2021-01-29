b595398d5cf3a654ab535d8e75bd1723adf961f1
watarmeli <watar@ukr.net>
2021-01-29 23:41:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,33 +8,44 @@ const atTheOldToad = {
   getPotions() {
     return this.potions;
   },
+
   addPotion(potionName) {
-  for (let i = 0; i < this.potions.length; i += 1) {       
-    if (this.potions[i].name === potionName.name) {
-    return `Зелье ${potionName.name} уже есть в инвентаре!`;
-    } 
-  }  
-  
-  this.potions.push(potionName);
-  return this.potions;
-	},
-  removePotion(potionName) {
-  for (let i = 0; i < this.potions.length; i += 1) {       
-    if (this.potions[i].name === potionName) {
-      this.potions.splice(i, 1)
-      return this.potions;
-    }
-  }
-  return `Зелья ${potionName} нет в инвентаре!`;
+
+    for (let i = 0; i < this.potions.length; i += 1) {       
+      if (this.potions[i].name === potionName.name) {
+      return `Зелье ${potionName.name} уже есть в инвентаре!`;
+      } 
+    }  
+    
+    this.potions.push(potionName);
+    return this.potions;
+    
   },
-  updatePotionName(oldName, newName) {
-  for (let i = 0; i < this.potions.length; i += 1) {
-    if (this.potions[i].name === oldName) {
-      this.potions[i].name = newName;
-      return this.potions;
+  
+  removePotion(potionName) {
+
+    for (let i = 0; i < this.potions.length; i += 1) {       
+      if (this.potions[i].name === potionName) {
+        this.potions.splice(i, 1)
+        return this.potions;
+      }
     }
-  }
-  return `Зелья ${oldName} нет в инвентаре!`  
+
+    return `Зелья ${potionName} нет в инвентаре!`;
+
+  },
+
+  updatePotionName(oldName, newName) {
+
+    for (let i = 0; i < this.potions.length; i += 1) {
+      if (this.potions[i].name === oldName) {
+        this.potions[i].name = newName;
+        return this.potions;
+      }
+    }
+
+    return `Зелья ${oldName} нет в инвентаре!` 
+    
   },
   // Пиши код выше этой строки
 };
